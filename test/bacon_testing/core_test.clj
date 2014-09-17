268d12524c0064368a8a2d456eaae0adbb2bb76d
Alex Kovar <ajkovar@gmail.com>
2014-09-17 14:20:26 -0500

    init

---
+++ test/bacon_testing/core_test.clj
@@ -0,0 +1,7 @@
+(ns bacon-testing.core-test
+  (:require [clojure.test :refer :all]
+            [bacon-testing.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
